390d98e5c0d5025f20d281be90f81d3bb91cf56e
QIBO <34388425+fathur-rahman@users.noreply.github.com>
2019-09-25 20:24:11 +0700

    Update 2019_09_25_065026_users_detail.php

--- database/migrations/2019_09_25_065026_users_detail.php
+++ database/migrations/2019_09_25_065026_users_detail.php
@@ -14,6 +14,9 @@ class UsersDetail extends Migration
     public function up()
     {
         //
+        Schema::create('users_detail', function (Blueprint $table) {
+
+        });
     }
 
     /**
