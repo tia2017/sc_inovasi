785eee8234fd832550e0489b1ad67240e88b49f7
Mahdiwidian <35493191+Mahdiwidian@users.noreply.github.com>
2019-10-02 14:52:55 +0700

    test test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
+    //kita push ya 
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Inovasi - Smart City</title>
