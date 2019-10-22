29715c2ee1d92e4131128c6198e94aa46cf034e6
caturbgs <caturbgs@github.com>
2019-10-22 08:35:46 +0700

    fungsi cari (belum fix)

--- app/Http/Controllers/inovasiController.php
+++ app/Http/Controllers/inovasiController.php
@@ -90,18 +90,18 @@ class inovasiController extends Controller
         }
         return redirect('inovasi')->with('status', 'Data Inovasi Berhasil Diubah');
     }
-    // public function update($id, Request $request){
-    //     $id_step = Innovation_step::where('innovation_id',$id)
-    //             ->get('id');
-    //     foreach($id_step as $id_s){
-    //         $data = Innovation_step::find($id_s);
+    public function search(Request $request){
+        $cari_global = $request->cari_global;
+        $cari_inovasi = $request->cari_inovasi;
+        $cari_perangkat = $request->cari_perangkat;
+        $cari_tahun = $request->cari_tahun;
+        $cari_bulan = $request->cari_bulan;
 
-    //             $data->progress_persentage = $request->progress_inovasi;
-    //             $data->explaination = $request->keterangan;
-
-    //             $data->save();
-    //     }
-
-    //     return redirect('inovasi')->with('status', 'Data Inovasi Berhasil Diubah');
-    // }
+        $inovasi = Innovation::
+              where('name', 'like', "%".$cari_inovasi."%")
+            ->where('short_name', 'like', "%".$cari_perangkat."%")
+            ->where('date', 'like', "%".$cari_tahun."%")
+            ->where('date', 'like', "%".$cari_bulan."%")
+            ->paginate();
+    }
 }
--- resources/views/tambahInovasi/index.blade.php
+++ resources/views/tambahInovasi/index.blade.php
@@ -19,10 +19,10 @@
                         <nav class="breadcrumb-container" aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item">
-                                    <a href="../index.html"><i class="ik ik-home"></i></a>
+                                    <a href="/dashboard"><i class="ik ik-home"></i></a>
                                 </li>
                                 <li class="breadcrumb-item">
-                                    <a href="#">Kelola</a>
+                                    <a href="/inovasi">Kelola</a>
                                 </li>
                                 <li class="breadcrumb-item active" aria-current="page">Tambah Inovasi</li>
                             </ol>
