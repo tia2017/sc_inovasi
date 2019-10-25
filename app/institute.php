bf89fb5094916bca8d6ecb6a40d74c7a1920c3c7
musyaffanq <50899323+musyaffanq@users.noreply.github.com>
2019-10-25 19:39:34 +0700

    inovasi perangkat daerah

--- app/institute.php
+++ app/institute.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class institute extends Model
 {
+	protected $table = 'institutes';
+
     public function innovation()
     {
         return $this->hasMany(Innovation::class);
