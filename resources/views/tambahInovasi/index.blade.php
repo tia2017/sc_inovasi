6e710f49f683c02b3e7cf9ae3a7ac99d4a1a6a51
ridwanramadhan <34785758+ridwanramadhan@users.noreply.github.com>
2019-10-30 10:19:33 +0700

    benerin yg error

--- resources/views/tambahInovasi/index.blade.php
+++ resources/views/tambahInovasi/index.blade.php
@@ -42,7 +42,7 @@
                         <div class="tab-content" id="myTabContent2">
 
                                 <div class="col-md-12">
-                                    <form class="forms-sample" action="/tambah-inovasi" method="post" enctype="multipart/form-data">
+                                    <form class="forms-sample" action="/tambah-inovasi" method="post">
                                     <input type="hidden" name="created_by" value="1">
                                     <input type="hidden" name="date" value="{{date('Y-m-d h:i:s')}}">
                                     <input type="hidden" name="verification_status" value="True">
@@ -184,19 +184,19 @@
                                                         <div class="clearfix"></div>
                                                         <!-- Repeater Items -->
                                                         <!--(bingung) mitra masih null -->
-                                                        <div class="items" data-group="test">
+                                                        <div class="items" data-group="mitra">
                                                             <!-- Repeater Content -->
                                                             <div class="item-content">
                                                                 <div class="form-group">
                                                                     <label for="bentukMitra" class="col-lg-2 col-md-2 control-label">Bentuk Mitra</label>
                                                                     <div class="col-lg-10 col-md-10">
-                                                                        <input type="text" class="form-control" id="bentukMitra" placeholder="Bentuk Mitra yang Melakukan Kerjasama" data-name="bentuk" name="form[]">
+                                                                        <input type="text" class="form-control " id="bentukMitra" placeholder="Bentuk Mitra yang Melakukan Kerjasama" data-name="Bentuk" value="{{old('Bentuk')}}" >                                                                        
                                                                     </div>
                                                                 </div>
                                                                 <div class="form-group">
                                                                     <label for="namaMitra" class="col-lg-2 control-label">Nama Mitra</label>
                                                                     <div class="col-lg-10">
-                                                                        <input type="text" class="form-control" id="namaMitra" placeholder="Nama Mitra yang Melakukan Kerjasama" data-name="nama" name="partner_name[]">
+                                                                        <input type="text" class="form-control" id="namaMitra" placeholder="Nama Mitra yang Melakukan Kerjasama" data-name="Nama">
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -422,7 +422,7 @@
                                                                 </div>
                                                                 <div class="clearfix"></div>
                                                                 <!-- Repeater Items -->
-                                                                <div class="items" data-group="test">
+                                                                <div class="items" data-group="mitra">
                                                                     <!-- Repeater Content -->
                                                                     <div class="item-content">
                                                                         <div class="form-group">
